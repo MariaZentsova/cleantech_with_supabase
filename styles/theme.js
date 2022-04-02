726fa249999979c579f04fed7f099f061e96b28f
MariaZentsova <mariazentsova@icloud.com>
2022-04-02 15:04:21 +0300

    added theme

---
+++ styles/theme.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { extendTheme, theme  } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: 'Fraktion Mono, monospace',
+  }
+});
+
+export default theme;
