2b16bd0cdc8e4393af0f2bc03ce6789d0c3b117f
MariaZentsova <mariazentsova@icloud.com>
2022-04-07 18:44:30 +0300

    updated imports

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import {
     NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY
-  } from "./constants";
+  } from "./Constants";
 
 export const supabase = createClient(
   NEXT_PUBLIC_SUPABASE_URL,
