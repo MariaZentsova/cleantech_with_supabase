bb050181f454e869f8b948630e14aaa4e18fce1f
MariaZentsova <mariazentsova@icloud.com>
2022-04-07 18:52:15 +0300

    removed user fetch

--- pages/cleantech-funding.js
+++ pages/cleantech-funding.js
@@ -2,13 +2,12 @@ import {
     Breadcrumb,
     BreadcrumbItem,
     BreadcrumbLink,
-
 } from '@chakra-ui/react'
 import Table from "@/components/Table"
 import AppShell from "@/components/AppShell";
-import { basePath } from "@/utils/siteConfig";
+//import { basePath } from "@/utils/siteConfig";
 
-const CleanTechFunding = ({user}) => {
+const CleanTechFunding = () => {
 
     return (
         <AppShell width="full" maxWidth="1280px" mx="auto" px={6} py={6}l>
@@ -26,26 +25,26 @@ const CleanTechFunding = ({user}) => {
     )
 }
 
-export async function getServerSideProps() {
-    // We need to implement `/api/getUser` by creating 
-    // an endpoint in `pages/api` but for now let's just call it
-  const response = await fetch(`${basePath}/api/getUser`).then((response) =>
-    response.json()
-  );
+// export async function getServerSideProps() {
+//     // We need to implement `/api/getUser` by creating 
+//     // an endpoint in `pages/api` but for now let's just call it
+//   const response = await fetch(`${basePath}/api/getUser`).then((response) =>
+//     response.json()
+//   );
 
-  const { user } = response;
+//   const { user } = response;
 
- // If the `getUser` endpoint doesn't have a user in its response
- // then we will redirect to the login page
- // which means this page will only be viewable when `getUser` returns a user.
+//  // If the `getUser` endpoint doesn't have a user in its response
+//  // then we will redirect to the login page
+//  // which means this page will only be viewable when `getUser` returns a user.
 
-  if (!user) {
-    return {
-      redirect: { destination: "/register", permanent: false },
-    };
-  }
-  // We'll pass the returned `user` to the page's React Component as a prop
-  return { props: { user } };
-}
+//   if (!user) {
+//     return {
+//       redirect: { destination: "/register", permanent: false },
+//     };
+//   }
+//   // We'll pass the returned `user` to the page's React Component as a prop
+//   return { props: { user } };
+// }
 
 export default CleanTechFunding
